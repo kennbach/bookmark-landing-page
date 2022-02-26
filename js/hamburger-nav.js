00769007ee6b42800e4b6c2f0b416ed484132ca1
Kenn Bach <3580672+kennbach@users.noreply.github.com>
2022-02-26 09:31:30 -0800

    added mobile header  and header nav

--- js/hamburger-nav.js
+++ js/hamburger-nav.js
@@ -1,12 +1,36 @@
 "use strict";
 
-// Get the header-nav__icon
-const img = document.getElementById("header-nav__icon");
+// Get the header-logo
+const logoBlue = document.getElementById("header-logo--blue");
+// Get the header-logo
+const logoWhite = document.getElementById("header-logo--white");
+// Get the header-nav__icon--hamburger
+const openIcon = document.getElementById("header-nav__icon--hamburger");
+// Get the header-nav__icon--close
+const closeIcon = document.getElementById("header-nav__icon--close");
 // Get the header-nav
 const nav = document.getElementById("header-nav");
 
-// Add event listener to header-nav__icon
-img.addEventListener("click", function () {
+// Add event listener to openIcon
+openIcon.addEventListener("click", function () {
   // Toggle the header-nav
-  nav.classList.toggle("header-nav--inactive");
+  nav.classList.toggle("display--none");
+  // Toggle the header logos
+  logoBlue.classList.toggle("display--none");
+  logoWhite.classList.toggle("display--none");
+  // Toggle the header nav icons
+  openIcon.classList.toggle("display--none");
+  closeIcon.classList.toggle("display--none");
+});
+
+// Add event listener to closeIcon
+closeIcon.addEventListener("click", function () {
+  // Toggle the header-nav
+  nav.classList.toggle("display--none");
+  // Toggle the header logos
+  logoBlue.classList.toggle("display--none");
+  logoWhite.classList.toggle("display--none");
+  // Toggle the header nav icons
+  openIcon.classList.toggle("display--none");
+  closeIcon.classList.toggle("display--none");
 });
